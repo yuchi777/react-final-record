2cc781c348bf84124552927fd88cdf8e42e93606
nick <yuchi8943.demo@gmail.com>
2025-11-03 17:34:39 +0800

    後台管理雛型完成加上前台版型套用

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Login from './pages/Login';
 import Dashboard from './pages/admin/Dashboard';
 import AdminProducts from './pages/admin/AdminProducts';
 import AdminCoupons from './pages/admin/AdminCoupons';
+import FrontLayout from './pages/front/FrontLayout';
+import Home from './pages/front/Home';
+import Products from './pages/front/Products';
 
 function App() {
 
@@ -28,6 +31,11 @@ function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<FrontLayout/>}>
+          <Route path="" element={<Home/>} />
+          <Route path="products" element={<Products/>} /> 
+        </Route>
+        
         {/* <Route path="/" element={<Navigate to="/login" replace />} /> */}
         <Route path="/login" element={<Login/>} />
         <Route path='/admin' element={<Dashboard/>}>
